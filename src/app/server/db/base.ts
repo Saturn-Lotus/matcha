6ce761a7033e331831333a2b3a8bbda694e506b5
async-jpx <del-khay@student.1337.ma>
2025-07-26 19:00:50 +0100

    feat(database): implement IBaseDB interface and PostgresDB class for database operations

---
+++ src/app/server/db/base.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export default interface IBaseDB {
+  /**
+   * Executes a SQL query and returns the result.
+   * @param text The SQL query string.
+   * @param params Optional parameters for the query.
+   * @returns A promise that resolves to the result of the query.
+   */
+  query(text: string, params?: any[]): Promise<any[]>;
+  /**
+   * Executes a SQL mutation and returns the result.
+   * @param text The SQL mutation string.
+   * @param params Optional parameters for the mutation.
+   * @returns A promise that resolves to the result of the mutation.
+   */
+  mutate(text: string, params?: any[]): Promise<any[]>;
+  /**
+   * Begins a transaction and executes the provided callback function with a transaction client.
+   * @param callback The function to execute within the transaction.
+   * @returns A promise that resolves to the result of the callback function.
+   */
+  // transaction<T = any>(callback: (client: IBaseDB) => Promise<T>): Promise<T>;
+}
+
+export interface IGraphDB extends IBaseDB {
+  /**
+   * Executes a Cypher query and returns the result.
+   * @param text The Cypher query string.
+   * @param params Optional parameters for the query.
+   * @returns A promise that resolves to the result of the query.
+   */
+  query(text: string, params?: any[]): Promise<any[]>;
+}
---
+++ src/app/server/db/postgres.ts
@@ -0,0 +1,38 @@
+import { Pool, QueryResultRow } from 'pg';
+import IBaseDB from './base';
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  min: 1,
+  idleTimeoutMillis: 30000, // 30 seconds
+  connectionTimeoutMillis: 2000, // 2 seconds
+  maxLifetimeSeconds: 60, // 1 minute
+});
+
+/**
+ * @param text SQL query string.
+ * @param params Optional parameters for the query.
+ * @returns A promise that resolves to the result of the query.
+ */
+export const query = (text: string, params: any[]) => {
+  return pool.query(text, params);
+};
+
+export class PostgresDB implements IBaseDB {
+  async query<M extends QueryResultRow = any>(
+    text: string,
+    params?: any[],
+  ): Promise<M[]> {
+    const client = await pool.connect();
+    const result = await client.query<M>(text, params);
+    client.release();
+    return result.rows;
+  }
+
+  async mutate<T = any>(text: string, params?: any[]): Promise<T> {
+    const client = await pool.connect();
+    const result = await client.query(text, params);
+    client.release();
+    return result.rows as T;
+  }
+}
