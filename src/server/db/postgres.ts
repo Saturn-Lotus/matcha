dabf9aabcc9b2d552f985ad1ab16bb321aa0a650
async-jpx <del-khay@student.1337.ma>
2025-11-08 18:58:28 +0100

    refactor(postgres): enhance connection management in PostgresDB class

--- src/server/db/postgres.ts
+++ src/server/db/postgres.ts
@@ -1,4 +1,4 @@
-import { Pool, QueryResultRow } from 'pg';
+import { Pool, PoolClient, QueryResultRow } from 'pg';
 import IBaseDB from './base';
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -19,20 +19,57 @@ export const query = (text: string, params: any[]) => {
 };
 
 export class PostgresDB implements IBaseDB {
+  private client: PoolClient | null = null;
+
+  constructor(client?: PoolClient) {
+    this.client = client || null;
+  }
+
   async query<M extends QueryResultRow = any>(
     text: string,
     params?: any[],
   ): Promise<M[]> {
-    const client = await pool.connect();
+    const client = this.client ?? (await pool.connect());
+    const shouldRelease = Boolean(this.client === null);
+
     const result = await client.query<M>(text, params);
-    client.release();
+    if (shouldRelease) {
+      client.release();
+      this.client = null;
+    }
     return result.rows;
   }
 
   async mutate<T = any>(text: string, params?: any[]): Promise<T> {
-    const client = await pool.connect();
+    const client = this.client ?? (await pool.connect());
+    const shouldRelease = Boolean(this.client === null);
+
     const result = await client.query(text, params);
-    client.release();
+    if (shouldRelease) {
+      client.release();
+      this.client = null;
+    }
     return result.rows as T;
+  }
+
+  async transaction<T>(
+    fn: (transactionDB: PostgresDB) => Promise<T>,
+    onError?: (error: Error) => void,
+  ): Promise<T> {
+    const client = await pool.connect();
+    const transactionDB = new PostgresDB(client);
+
+    try {
+      await client.query('BEGIN');
+      const result = await fn(transactionDB);
+      await client.query('COMMIT');
+      return result;
+    } catch (error) {
+      await client.query('ROLLBACK');
+      onError?.(error as Error);
+      throw error;
+    } finally {
+      client.release();
+    }
   }
 }
